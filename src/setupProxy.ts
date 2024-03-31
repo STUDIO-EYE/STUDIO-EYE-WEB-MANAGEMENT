25a54e56179bfc29b8d73b49c008911f6b8d5aa8
sunminnnnn <sslspplz@mju.ac.kr>
2024-03-31 21:21:10 +0900

    [Build] 2nd spring 배포 빌드

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -3,14 +3,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app: any){
     app.use(
         createProxyMiddleware('/api', {
-            target: 'http://3.36.120.219:8080',
+            target: 'http://43.201.95.252:8000',
             changeOrigin: true
         })
     )
 
     app.use(
         createProxyMiddleware( '/user-service', {
-            target: 'http://43.203.217.95:8080',
+            target: 'http://43.201.95.252:8000',
             changeOrigin: true
         })
     );
