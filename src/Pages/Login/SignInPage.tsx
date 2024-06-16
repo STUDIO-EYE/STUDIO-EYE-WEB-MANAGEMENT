9cb49578f7d0e8dae129b943842f10918d149435
sunminnnnn <sslspplz@mju.ac.kr>
2024-06-16 14:41:54 +0900

    [build] cicd 확인용2

--- src/Pages/Login/SignInPage.tsx
+++ src/Pages/Login/SignInPage.tsx
@@ -256,7 +256,7 @@ function SignInPage() {
             <SignInBox className="SignInBox">
                 <SubInputForm className="SubInputFormWithButton">
                     {loading &&
-                        <TextSm style={{ color: 'darkgray' }}>로딩 중...</TextSm>
+                        <TextSm style={{ color: 'gray' }}>로딩 중...</TextSm>
                     }
                     <TextMd>EMAIL</TextMd>
                     <HorizontalBox>
